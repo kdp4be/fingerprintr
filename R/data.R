1c808dd89239e9e334373b3fc64bb4dcb095b5d9
Karen Pan <kdp4be@virginia.edu>
2019-05-01 16:43:59 -0400

    data.R file, roxygenise updates
    add data.R file, rerun reoxygenise, edit vignette

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' G080 minutiae information.
+#'
+#' X and Y pixel locations for latent G080.
+#'
+#' @format A matrix with 21 minutiae pixel locations.
+#' \describe{
+#' \item{X}{X pixel value location}
+#' \item{Y}{Y pixel value location}
+#' }
+#' @examples
+#'   G080_min
+"G080_min"
